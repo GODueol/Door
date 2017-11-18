fafae293b53791d255d4383ae4f2d9b318e24a45
Kwon <rnjscjfgur@naver.com>
2017-11-18 19:35:38 +0900

    기본 폰트 넣음

--- app/src/main/java/com/example/kwoncheolhyeok/core/Util/CustomFontApp.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/Util/CustomFontApp.java
@@ -11,7 +11,14 @@ public class CustomFontApp extends Application {
         Typekit.getInstance()
                 .addNormal(Typekit.createFromAsset(this, "DroidSans.ttf"))
                 .addBold(Typekit.createFromAsset(this, "DroidSans-Bold.ttf"))
-                .addCustom1(Typekit.createFromAsset(this, "FuturaStd-Medium.otf"));
+                .addCustom1(Typekit.createFromAsset(this, "Florence-Regular.ttf"))
+
+//                .addItalic(Typekit.createFromAsset(this, "OpenSans-Italic.ttf"))
+//                .addBoldItalic(Typekit.createFromAsset(this, "OpenSans-BoldItalic.ttf"))
+//                .addCustom1(Typekit.createFromAsset(this, "NanumSquareR.ttf"))
+//                .addCustom2(Typekit.createFromAsset(this, "NanumSquareB.ttf"));
+//
+        ;
 
     }
 }
