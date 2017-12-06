6435367137507ca34516ecf1a14cd39da18b3609
ByoungJin <kbj2043@gmail.com>
2017-12-06 21:33:10 +0900

    ANDROID 19에서 죽는 에러 해결 사진 파일 폴더를 v2 > m* 로 변경

--- app/src/main/java/com/example/kwoncheolhyeok/core/PeopleFragment/FullImageViewPager/PagerPage.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/PeopleFragment/FullImageViewPager/PagerPage.java
@@ -1,22 +1,14 @@
 package com.example.kwoncheolhyeok.core.PeopleFragment.FullImageViewPager;
 
 import android.annotation.TargetApi;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.example.kwoncheolhyeok.core.R;
 import com.github.chrisbanes.photoview.PhotoView;
 
@@ -39,26 +31,9 @@ public class PagerPage extends android.support.v4.app.Fragment {
             String picPath = getArguments().getString("picPath");
             scrollView = view.findViewById(R.id.scrollView);
             imageView = view.findViewById(R.id.pictureImage);
-            final RelativeLayout background = view.findViewById(R.id.background);
 
             Glide.with(imageView.getContext() /* context */)
-                    .load(picPath).listener(new RequestListener<Drawable>() {
-                @Override
-                public boolean onLoadFailed(@Nullable GlideException e, Object o, Target<Drawable> target, boolean b) {
-                    return false;
-                }
-
-                @Override
-                public boolean onResourceReady(Drawable drawable, Object o, Target<Drawable> target, DataSource dataSource, boolean b) {
-                    // 이미지 파일의 가로 세로 비율에 맞게 이미지 크기를 늘림
-                    int width = drawable.getIntrinsicWidth();
-                    int height = drawable.getIntrinsicHeight();
-                    int pWidth = background.getMeasuredWidth();
-                    imageView.getLayoutParams().width = pWidth;
-                    imageView.getLayoutParams().height = height * pWidth / width;
-                    return false;
-                }
-            }).into(imageView);
+                    .load(picPath).into(imageView);
 
         }
         return view;
