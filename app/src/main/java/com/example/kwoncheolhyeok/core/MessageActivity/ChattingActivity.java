9f676d8aed75bc43bbe5d12d8680e697a1e2de8f
godueol <weogjw01@naver.com>
2017-12-23 16:08:44 +0900

    edit] 채팅 프로토타입

--- app/src/main/java/com/example/kwoncheolhyeok/core/MessageActivity/ChattingActivity.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/MessageActivity/ChattingActivity.java
@@ -1,9 +1,11 @@
 package com.example.kwoncheolhyeok.core.MessageActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -12,8 +14,10 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.kwoncheolhyeok.core.MessageActivity.chat_message_view.util.MessageVO;
+import com.example.kwoncheolhyeok.core.PeopleFragment.ImageAdapter;
 import com.example.kwoncheolhyeok.core.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,9 +37,10 @@ public class ChattingActivity extends AppCompatActivity {
 
     private ChatMessageAdapter mAdapter;
 
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabase,userRef;
     private FirebaseAuth mAuth;
     private String userId;
+    private String roomName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +55,7 @@ public class ChattingActivity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_36dp);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
         mAuth = FirebaseAuth.getInstance();
         userId =  mAuth.getUid();
 
@@ -61,6 +67,16 @@ public class ChattingActivity extends AppCompatActivity {
         mAdapter = new ChatMessageAdapter(this, new ArrayList<ChatMessage>());
         mListView.setAdapter(mAdapter);
 
+        Intent p = getIntent();
+        final ImageAdapter.Item item = (ImageAdapter.Item) p.getSerializableExtra("user");
+        String targetuuid = item.getUuid();
+        // userId 내아이디 , item.getUuid() 상대방 아이디
+       // Toast.makeText(this,        userRef.child(userId).child("chatRoom").child(targetuuid).getKey(),Toast.LENGTH_SHORT).show();
+        userRef = FirebaseDatabase.getInstance().getReference("users");
+        DatabaseReference roomKeyRef = userRef.child(userId).child("chatRoom").child(targetuuid).push();
+        roomName = roomKeyRef.getKey();
+        userRef.child(userId).child("chatRoom").child(targetuuid).setValue(roomName);
+        userRef.child(targetuuid).child("chatRoom").child(userId).setValue(roomName);
 
         mButtonSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,9 +85,7 @@ public class ChattingActivity extends AppCompatActivity {
                 if (TextUtils.isEmpty(message)) {
                     return;
                 }
-                writeMessage(userId,0,"kaikai",message,0);
-                //newMessage();
-                sendMessage(message);
+                writeMessage(roomName,0, userId,"kaikai",message,0);
                 mEditTextMessage.setText("");
             }
         });
@@ -84,15 +98,14 @@ public class ChattingActivity extends AppCompatActivity {
         });
 
 
+        FirebaseDatabase.getInstance().getReference().child("message").child(roomName)
+                .addChildEventListener(childEventListener);
     }
 
-    private void sendMessage(String message) {
-        ChatMessage chatMessage = new ChatMessage(message, true, false);
-        mAdapter.add(chatMessage);
-        writeMessage(userId,0,"kaikai",message,0);
-        mimicOtherMessage(message);
+    private void writeMessage(String room , int img,String userId, String nickname, String content, int editimg) {
+        MessageVO message = new MessageVO(img,userId ,nickname, content,editimg);
+        mDatabase.child("message").child(room).push().setValue(message);
     }
-
     private void mimicOtherMessage(String message) {
         ChatMessage chatMessage = new ChatMessage(message, false, false);
         mAdapter.add(chatMessage);
@@ -110,19 +123,70 @@ public class ChattingActivity extends AppCompatActivity {
         mAdapter.add(chatMessage);
     }
 
-    private void writeMessage(String userId, int img, String nickname, String content, int editimg) {
-        MessageVO message = new MessageVO(img,nickname, content,editimg);
+    ChildEventListener childEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+            Log.d("fireabaseUpdate", "onChildAdded:" + dataSnapshot.getKey());
 
-        mDatabase.child("message").child(userId).setValue(message);
-    }
+            // A new comment has been added, add it to the displayed list
+            MessageVO user = dataSnapshot.getValue(MessageVO.class);
+            String message = user.getContent();
+            ChatMessage chatMessage = new ChatMessage(message, true, false);
+            mAdapter.add(chatMessage);
+            // ...
+        }
 
-    private void newMessage() {
-        FirebaseDatabase.getInstance().getReference().child("users").child(userId)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+            Log.d("fireabaseUpdate", "onChildChanged:" + dataSnapshot.getKey());
+
+            // A comment has changed, use the key to determine if we are displaying this
+            // comment and if so displayed the changed comment.
+            MessageVO user = dataSnapshot.getValue(MessageVO.class);
+            String commentKey = dataSnapshot.getKey();
+
+            // ...
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            Log.d("fireabaseUpdate", "onChildRemoved:" + dataSnapshot.getKey());
+
+            // A comment has changed, use the key to determine if we are displaying this
+            // comment and if so remove it.
+            String commentKey = dataSnapshot.getKey();
+
+            // ...
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+            Log.d("fireabaseUpdate", "onChildMoved:" + dataSnapshot.getKey());
+
+            // A comment has changed position, use the key to determine if we are
+            // displaying this comment and if so move it.
+            MessageVO user = dataSnapshot.getValue(MessageVO.class);
+            String commentKey = dataSnapshot.getKey();
+
+            // ...
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            Log.w("fireabaseUpdate", "postComments:onCancelled", databaseError.toException());
+            Toast.makeText(getApplicationContext(), "Failed to load comments.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+        /*addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         // Get user information
+
                         MessageVO user = dataSnapshot.getValue(MessageVO.class);
+                        String message = user.getContent();
+                        ChatMessage chatMessage = new ChatMessage(message, true, false);
+                        mAdapter.add(chatMessage);
                        // sendMessage(user.getContent());
                     }
 
@@ -131,7 +195,7 @@ public class ChattingActivity extends AppCompatActivity {
 
                     }
                 });
-    }
+    }*/
 //    private void scrollMyListViewToBottom() {
 //        mListView.post(new Runnable() {
 //            @Override
