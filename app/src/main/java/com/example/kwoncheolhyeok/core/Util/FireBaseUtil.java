d4b154d006d51e64a6768cdffaa64123d329450b
ByoungJin <kbj2043@gmail.com>
2017-12-28 17:16:25 +0900

    친구 추가 기능, 데이터 꼬일 부분 해결

--- app/src/main/java/com/example/kwoncheolhyeok/core/Util/FireBaseUtil.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/Util/FireBaseUtil.java
@@ -31,7 +31,7 @@ public class FireBaseUtil {
     }
 
 
-    public Task<Void> follow(final User oUser, String uuid , boolean isFollow, String myUuid) {
+    public Task<Void> follow(final User oUser, String oUuid , boolean isFollow, String myUuid) {
         final User mUser = DataContainer.getInstance().getUser();
         //final User oUser = item.getUser();
         DatabaseReference mDatabase = DataContainer.getInstance().getUsersRef();
@@ -39,39 +39,41 @@ public class FireBaseUtil {
 
         if(isFollow){
             // 친구 삭제
-            if(!mUser.getFollowingUsers().containsKey(uuid)) return null;
-            mUser.getFollowingUsers().remove(uuid);
-            childUpdates.put("/" + myUuid + "/followingUsers", mUser.getFollowingUsers());
+            if(!mUser.getFollowingUsers().containsKey(oUuid)) return null;
+
+            mUser.getFollowingUsers().remove(oUuid);
+            childUpdates.put("/" + myUuid + "/followingUsers/" + oUuid, null);
 
             oUser.getFollowerUsers().remove(myUuid);
-            childUpdates.put("/" + uuid + "/followerUsers", oUser.getFollowerUsers());
+            childUpdates.put("/" + oUuid + "/followerUsers/" + myUuid, null);
 
             // 상대방이 팔로우 되어있으면 친구 삭제
-            if(mUser.getFriendUsers().containsKey(uuid)) {
-                mUser.getFriendUsers().remove(uuid);
-                childUpdates.put("/" + myUuid + "/friendUsers", mUser.getFollowingUsers());
+            if(mUser.getFriendUsers().containsKey(oUuid)) {
+                mUser.getFriendUsers().remove(oUuid);
+                childUpdates.put("/" + myUuid + "/friendUsers/" + oUuid, null);
 
                 oUser.getFriendUsers().remove(myUuid);
-                childUpdates.put("/" + uuid + "/friendUsers", oUser.getFollowerUsers());
+                childUpdates.put("/" + oUuid + "/friendUsers/" + myUuid, null);
             }
 
         } else {
             // 친구 추가
-            if(mUser.getFollowingUsers().containsKey(uuid)) return null;
+            if(mUser.getFollowingUsers().containsKey(oUuid)) return null;
             long now = System.currentTimeMillis();
-            mUser.getFollowingUsers().put(uuid, now);
-            childUpdates.put("/" + myUuid + "/followingUsers", mUser.getFollowingUsers());
+
+            mUser.getFollowingUsers().put(oUuid, now);
+            childUpdates.put("/" + myUuid + "/followingUsers/" + oUuid, now);
 
             oUser.getFollowerUsers().put(myUuid, now);
-            childUpdates.put("/" + uuid + "/followerUsers", oUser.getFollowerUsers());
+            childUpdates.put("/" + oUuid + "/followerUsers/" + myUuid, now);
 
             // 상대방이 팔로우 되어있으면 친구에 추가
             if(oUser.getFollowingUsers().containsKey(myUuid)){
-                mUser.getFriendUsers().put(uuid, now);
-                childUpdates.put("/" + myUuid + "/friendUsers", mUser.getFollowingUsers());
+                mUser.getFriendUsers().put(oUuid, now);
+                childUpdates.put("/" + myUuid + "/friendUsers/" + oUuid, now);
 
                 oUser.getFriendUsers().put(myUuid, now);
-                childUpdates.put("/" + uuid + "/friendUsers", oUser.getFollowerUsers());
+                childUpdates.put("/" + oUuid + "/friendUsers/" + myUuid, now);
 
             }
         }
