1a13dd37712ed2e28afefc747f1bb9aaf8294a7e
Kwon <rnjscjfgur@naver.com>
2018-01-05 20:17:13 +0900

    세팅 리스트 액티비티 연결

---
+++ app/src/main/java/com/example/kwoncheolhyeok/core/SettingActivity/AccountActivity.java
@@ -0,0 +1,52 @@
+package com.example.kwoncheolhyeok.core.SettingActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import com.example.kwoncheolhyeok.core.R;
+
+/**
+ * Created by Kwon on 2018-01-04.
+ */
+
+public class AccountActivity extends AppCompatActivity {
+
+    Toolbar toolbar = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_account_activity);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 툴바 뒤로가기 버튼
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        getSupportActionBar().setDisplayShowHomeEnabled(true); //홈 아이콘을 숨김처리합니다.
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_36dp);
+
+
+
+
+    }
+
+
+    // 뒤로가기 버튼 기능
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // NavUtils.navigateUpFromSameTask(this);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
