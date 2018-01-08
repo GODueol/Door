fad6ab92d2c34ea6bd8c714a4e8e447a1d07e1d0
GODueol <weogjw01@naver.com>
2018-01-08 13:52:17 +0900

    edit] chat ver 1.5 (refactoring)

--- app/src/main/java/com/example/kwoncheolhyeok/core/MessageActivity/ChattingActivity.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/MessageActivity/ChattingActivity.java
@@ -1,15 +1,11 @@
 package com.example.kwoncheolhyeok.core.MessageActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -18,34 +14,17 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.kwoncheolhyeok.core.Entity.User;
-import com.example.kwoncheolhyeok.core.MessageActivity.chat_message_view.util.CryptoImeageName;
+import com.example.kwoncheolhyeok.core.MessageActivity.chat_message_view.util.ChatFirebaseUtil;
 import com.example.kwoncheolhyeok.core.MessageActivity.chat_message_view.util.MessageVO;
-import com.example.kwoncheolhyeok.core.MessageActivity.chat_message_view.util.RoomVO;
 import com.example.kwoncheolhyeok.core.PeopleFragment.ImageAdapter;
 import com.example.kwoncheolhyeok.core.R;
 import com.example.kwoncheolhyeok.core.Util.Camera.LoadPicture;
 import com.example.kwoncheolhyeok.core.Util.DataContainer;
-import com.example.kwoncheolhyeok.core.Util.FireBaseUtil;
-import com.example.kwoncheolhyeok.core.Util.GPSInfo;
-import com.firebase.geofire.GeoFire;
-import com.firebase.geofire.GeoLocation;
-import com.firebase.geofire.LocationCallback;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ChattingActivity extends AppCompatActivity {
 
@@ -63,17 +42,18 @@ public class ChattingActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     private String roomName;
+    ChatFirebaseUtil chatFirebaseUtil;
 
     private User user;
-    private String userNickName, userUuid, userPickuri;
+    private String  userUuid, userPickuri;
     private User targetUser;
-    private String targetNickName, targetUuid, targetPicuri;
+    private String targetUuid, targetPicuri;
     private  ImageAdapter.Item item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_chatting_activity);
-
+        Intent p = getIntent();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -86,17 +66,25 @@ public class ChattingActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-
         mListView = (ListView) findViewById(R.id.listView);
         mButtonSend = (ImageButton) findViewById(R.id.btn_send);
         mEditTextMessage = (EditText) findViewById(R.id.et_message);
         mImageView = (ImageButton) findViewById(R.id.iv_image);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
 
         mAdapter = new ChatMessageAdapter(this, new ArrayList<ChatMessage>());
         mListView.setAdapter(mAdapter);
 
-        checkchatRoom();
+        // 상대방 데이터 셋
+        targetUser = (User) p.getSerializableExtra("user");
+        targetUuid = (String) p.getSerializableExtra("userUuid");
+        targetPicuri = (String) p.getSerializableExtra("userPicuri");
+        // 내정보 데이터 셋
+        user = DataContainer.getInstance().getUser();
+        userUuid = mAuth.getUid();
+        userPickuri = user.getPicUrls().getPicUrl1();
+
+        chatFirebaseUtil = new ChatFirebaseUtil(this,user,targetUser,userUuid,targetUuid);
+        chatFirebaseUtil.checkchatRoom(mAdapter);
 
         // 메세지 보내기
         mButtonSend.setOnClickListener(new View.OnClickListener() {
@@ -129,38 +117,7 @@ public class ChattingActivity extends AppCompatActivity {
             if (requestCode == REQUEST_GALLERY) {
 
                 Uri outputFileUri = data.getData();
-
-                FirebaseStorage storage = FirebaseStorage.getInstance("gs://core-865fc.appspot.com");
-                StorageReference imagesRef = storage.getReference().child("message/");
-                long currentTime = System.currentTimeMillis();
-                final String imageName = CryptoImeageName.md5(Long.toString(currentTime));
-                final StorageReference imageMessageRef = imagesRef.child("image/"+roomName+"/"+userUuid+"/"+imageName);
-                UploadTask uploadTask = imageMessageRef.putFile(outputFileUri);
-
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("chatError",e.getMessage());
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Uri downloadUri = taskSnapshot.getDownloadUrl();
-                        imageMessageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                long currentTime = System.currentTimeMillis();
-                                writeMessage(roomName, uri.toString(), userUuid, user.getId(), null, currentTime, 1);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception exception) {
-                                // Handle any errors
-                            }
-                        });
-
-                    }
-                });
+                chatFirebaseUtil.sendImageMessage(outputFileUri);
 
             }
         }
@@ -168,185 +125,13 @@ public class ChattingActivity extends AppCompatActivity {
 
     private void writeMessage(String room, String image, String userId, String nickname, String content, long currentTime,int check) {
         MessageVO message = new MessageVO(image, userId, nickname, content,currentTime, check);
-        String key = mDatabase.child("chat").child(room).push().getKey();
-
-        Map<String, Object> childUpdates = new HashMap<>();
-        if(image == null) {
-            childUpdates.put("/chat/" + room + "/" + key, message);
-
-            childUpdates.put("/chatRoomList/" + targetUuid + "/" + userUuid + "/" + "lastChatTime", currentTime);
-            childUpdates.put("/chatRoomList/" + targetUuid + "/" + userUuid + "/" + "lastChat", content);
-
-            childUpdates.put("/chatRoomList/" + userUuid + "/" + targetUuid + "/" + "lastViewTime", currentTime);
-            childUpdates.put("/chatRoomList/" + userUuid + "/" + targetUuid + "/" + "lastChat", content);
-        }else if(content == null){
-            childUpdates.put("/chat/" + room + "/" + key, message);
-
-            childUpdates.put("/chatRoomList/" + targetUuid + "/" + userUuid + "/" + "lastChatTime", currentTime);
-            childUpdates.put("/chatRoomList/" + targetUuid + "/" + userUuid + "/" + "lastChat", "사진");
-            childUpdates.put("/chatRoomList/" + userUuid + "/" + targetUuid + "/" + "lastViewTime", currentTime);
-            childUpdates.put("/chatRoomList/" + userUuid + "/" + targetUuid + "/" + "lastChat", "사진");
-        }
-        mDatabase.updateChildren(childUpdates);
-    }
-
-    private void mimicOtherMessage(MessageVO message) {
-        ChatMessage chatMessage = new ChatMessage(message, false, false);
-        mAdapter.add(chatMessage);
+        chatFirebaseUtil.sendMessage(message);
     }
 
     private void sendImageMessage() {
         LoadPicture loadPicture = new LoadPicture(this, this);
         loadPicture.onGallery();
     }
-
-    private void mimicOtherMessage() {
-        ChatMessage chatMessage = new ChatMessage(null, false, true);
-        mAdapter.add(chatMessage);
-    }
-
-    public void checkchatRoom() {
-        Intent p = getIntent();
-        // 상대방 데이터 셋
-        targetUser = (User) p.getSerializableExtra("user");
-        targetUuid = (String) p.getSerializableExtra("userUuid");
-        targetPicuri = (String) p.getSerializableExtra("userPicuri");
-        //targetNickName = targetUser.getId();
-        // 내정보 데이터 셋
-        user = DataContainer.getInstance().getUser();
-        userUuid = mAuth.getUid();
-        userPickuri = user.getPicUrls().getPicUrl1();
-        //userNickName = user.getId();
-
-        chatRoomRef = FirebaseDatabase.getInstance().getReference("chatRoomList");
-
-        chatRoomRef.child(userUuid).child(targetUuid).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                RoomVO checkRoomVO = dataSnapshot.getValue(RoomVO.class);
-                long currentTime = System.currentTimeMillis();
-                try {
-                    roomName = checkRoomVO.getChatRoomid();
-                    chatRoomRef.child(userUuid).child(targetUuid).child("lastViewTime").setValue(currentTime);
-                } catch (Exception e) {
-                    // 채팅방 이름 설정
-                    roomName  = FirebaseDatabase.getInstance().getReference("chat").push().getKey();
-
-                    Map<String, Object> childUpdates = new HashMap<>();
-                    RoomVO roomVO = new RoomVO(roomName,null, userUuid,currentTime, userPickuri);
-                    childUpdates.put("/chatRoomList/" + targetUuid + "/" + userUuid  , roomVO);
-                    roomVO = new RoomVO(roomName,null, targetUuid,currentTime, targetPicuri);
-                    childUpdates.put("/chatRoomList/" + userUuid + "/" + targetUuid , roomVO);
-                    mDatabase.updateChildren(childUpdates);
-                    chatRoomRef.child(userUuid).child(targetUuid).child("lastViewTime").setValue(currentTime);
-                }
-                item = new ImageAdapter.Item(0,targetUuid,targetUser,targetPicuri);
-                // 상대방과의 거리 셋팅
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference(FireBaseUtil.currentLocationPath);
-                GeoFire geoFire = new GeoFire(ref);
-                final Location location = GPSInfo.getmInstance(getApplication()).getGPSLocation();
-                geoFire.getLocation(targetUuid, new LocationCallback() {
-                    @SuppressLint("DefaultLocale")
-                    @Override
-                    public void onLocationResult(String s, GeoLocation geoLocation) {
-                        Location targetLocation = new Location("");//provider name is unnecessary
-                        targetLocation.setLatitude(geoLocation.latitude);//your coords of course
-                        targetLocation.setLongitude(geoLocation.longitude);
-                        final float distance = location.distanceTo(targetLocation);
-                        item.setDistance(distance);
-                        //Log.d("distancec",String.format("%.1f", distance / 1000));
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
-
-
-                FirebaseDatabase.getInstance().getReference().child("chat").child(roomName)
-                        .addChildEventListener(childEventListener);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    ChildEventListener childEventListener = new ChildEventListener() {
-        @Override
-        public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
-            Log.d("fireabaseUpdate", "onChildAdded:" + dataSnapshot.getKey());
-
-            // A new comment has been added, add it to the displayed list
-            MessageVO message = dataSnapshot.getValue(MessageVO.class);
-            ChatMessage chatMessage;
-            int check = message.getCheck();
-
-            if(check!=0 && !message.getWriter().equals(userUuid)){
-                message.setCheck(0);
-                mDatabase.child("chat").child(roomName).child(dataSnapshot.getKey()).child("check").setValue(check-1);
-            }
-
-            if(message.getWriter().equals(userUuid) && message.getImage() == null) {
-                chatMessage = new ChatMessage(message, true, false);
-            }
-            else if(!message.getWriter().equals(userUuid) && message.getImage() == null){
-                chatMessage = new ChatMessage(message, false, false, item);
-            }
-            else if(message.getWriter().equals(userUuid) && message.getContent() == null){
-                chatMessage = new ChatMessage(message, true, true);
-            }
-            else{
-                chatMessage = new ChatMessage(message, false, true,item);
-            }
-            mAdapter.add(chatMessage);
-            // ...
-        }
-
-        @Override
-        public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
-            Log.d("fireabaseUpdate", "onChildChanged:" + dataSnapshot.getKey());
-
-            // A comment has changed, use the key to determine if we are displaying this
-            // comment and if so displayed the changed comment.
-            MessageVO user = dataSnapshot.getValue(MessageVO.class);
-            String commentKey = dataSnapshot.getKey();
-            mAdapter.notifyDataSetChanged();
-            // ...
-        }
-
-        @Override
-        public void onChildRemoved(DataSnapshot dataSnapshot) {
-            Log.d("fireabaseUpdate", "onChildRemoved:" + dataSnapshot.getKey());
-
-            // A comment has changed, use the key to determine if we are displaying this
-            // comment and if so remove it.
-            String commentKey = dataSnapshot.getKey();
-
-            // ...
-        }
-
-        @Override
-        public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
-            Log.d("fireabaseUpdate", "onChildMoved:" + dataSnapshot.getKey());
-
-            // A comment has changed position, use the key to determine if we are
-            // displaying this comment and if so move it.
-            MessageVO user = dataSnapshot.getValue(MessageVO.class);
-            String commentKey = dataSnapshot.getKey();
-
-            // ...
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-            Log.w("fireabaseUpdate", "postComments:onCancelled", databaseError.toException());
-            Toast.makeText(getApplicationContext(), "Failed to load comments.",
-                    Toast.LENGTH_SHORT).show();
-        }
-    };
 
     // 뒤로가기 버튼 기능
     public boolean onOptionsItemSelected(android.view.MenuItem item) {
---
+++ app/src/main/java/com/example/kwoncheolhyeok/core/MessageActivity/chat_message_view/util/ChatFirebaseUtil.java
@@ -0,0 +1,245 @@
+package com.example.kwoncheolhyeok.core.MessageActivity.chat_message_view.util;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.kwoncheolhyeok.core.Entity.User;
+import com.example.kwoncheolhyeok.core.MessageActivity.ChatMessage;
+import com.example.kwoncheolhyeok.core.MessageActivity.ChatMessageAdapter;
+import com.example.kwoncheolhyeok.core.PeopleFragment.ImageAdapter;
+import com.example.kwoncheolhyeok.core.Util.FireBaseUtil;
+import com.example.kwoncheolhyeok.core.Util.GPSInfo;
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.LocationCallback;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2018-01-08.
+ */
+
+public class ChatFirebaseUtil {
+
+    final static String chatRoomList = "chatRoomList";
+    final static String chat = "chat";
+    final static String image = "image";
+    final static  String buckit = "gs://core-865fc.appspot.com";
+
+    Context context;
+    String userUuid, targetUuid;
+    User currentUser,targetUser;
+    String userPickuri, targetPicuri;
+    String roomName;
+    DatabaseReference databaseRef;
+    FirebaseStorage storage;
+
+    public ChatFirebaseUtil(Context context,User currentUser,User targetUser, String userUuid, String targetUuid) {
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+        storage = FirebaseStorage.getInstance(buckit);
+        this.context = context;
+        this.currentUser = currentUser;
+        this.targetUser = targetUser;
+        this.userUuid = userUuid;
+        this.targetUuid = targetUuid;
+        userPickuri = currentUser.getPicUrls().getPicUrl1();
+        targetPicuri = targetUser.getPicUrls().getPicUrl1();
+
+    }
+
+    public void sendMessage(MessageVO message) {
+
+        String room = roomName;
+        String key = databaseRef.child(chat).child(room).push().getKey();
+
+        Long currentTime = getTime();
+        Map<String, Object> childUpdates = new HashMap<>();
+
+        if (message.getImage() == null) {
+            childUpdates.put("/"+chat+"/"+ room + "/" + key, message);
+
+            childUpdates.put("/"+chatRoomList+"/"+ targetUuid + "/" + userUuid + "/" + "lastChatTime", currentTime);
+            childUpdates.put("/"+chatRoomList+"/"+ targetUuid + "/" + userUuid + "/" + "lastChat", message.getContent());
+
+            childUpdates.put("/"+chatRoomList+"/"+ userUuid + "/" + targetUuid + "/" + "lastViewTime", currentTime);
+            childUpdates.put("/"+chatRoomList+"/"+ userUuid + "/" + targetUuid + "/" + "lastChat", message.getContent());
+        } else if (message.getContent() == null) {
+            childUpdates.put("/"+chat+"/" + room + "/" + key, message);
+
+            childUpdates.put("/"+chatRoomList+"/"+ targetUuid + "/" + userUuid + "/" + "lastChatTime", currentTime);
+            childUpdates.put("/"+chatRoomList+"/" + targetUuid + "/" + userUuid + "/" + "lastChat", "사진");
+            childUpdates.put("/"+chatRoomList+"/" + userUuid + "/" + targetUuid + "/" + "lastViewTime", currentTime);
+            childUpdates.put("/"+chatRoomList+"/" + userUuid + "/" + targetUuid + "/" + "lastChat", "사진");
+        }
+        databaseRef.updateChildren(childUpdates);
+    }
+
+    public void sendImageMessage(Uri outputFileUri){
+
+        StorageReference imagesRef = storage.getReference().child(chat);
+        long currentTime = System.currentTimeMillis();
+        final String imageName = CryptoImeageName.md5(Long.toString(currentTime));
+        final StorageReference imageMessageRef = imagesRef.child(image+"/"+roomName+"/"+userUuid+"/"+imageName);
+        UploadTask uploadTask = imageMessageRef.putFile(outputFileUri);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("chatError",e.getMessage());
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUri = taskSnapshot.getDownloadUrl();
+                imageMessageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        long currentTime = getTime();
+                        MessageVO message = new MessageVO(uri.toString(), userUuid, currentUser.getId(), null,currentTime, 1);
+                        sendMessage(message);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                    }
+                });
+
+            }
+        });
+    }
+
+    public void checkchatRoom(final ChatMessageAdapter mAdapter) {
+        final  DatabaseReference chatRoomRef = FirebaseDatabase.getInstance().getReference(chatRoomList);
+
+        chatRoomRef.child(userUuid).child(targetUuid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                RoomVO checkRoomVO = dataSnapshot.getValue(RoomVO.class);
+                long currentTime = System.currentTimeMillis();
+                try {
+                    roomName = checkRoomVO.getChatRoomid();
+                    chatRoomRef.child(userUuid).child(targetUuid).child("lastViewTime").setValue(currentTime);
+                } catch (Exception e) {
+                    // 채팅방 이름 설정
+                    roomName  = FirebaseDatabase.getInstance().getReference(chat).push().getKey();
+
+                    Map<String, Object> childUpdates = new HashMap<>();
+                    RoomVO roomVO = new RoomVO(roomName,null, userUuid,currentTime, userPickuri);
+                    childUpdates.put("/"+chatRoomList+"/" + targetUuid + "/" + userUuid  , roomVO);
+                    roomVO = new RoomVO(roomName,null, targetUuid,currentTime, targetPicuri);
+                    childUpdates.put("/"+chatRoomList+"/" + userUuid + "/" + targetUuid , roomVO);
+                    databaseRef.updateChildren(childUpdates);
+                    chatRoomRef.child(userUuid).child(targetUuid).child("lastViewTime").setValue(currentTime);
+                }
+                final ImageAdapter.Item item = new ImageAdapter.Item(0,targetUuid,targetUser,targetPicuri);
+                // 상대방과의 거리 셋팅
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference(FireBaseUtil.currentLocationPath);
+                GeoFire geoFire = new GeoFire(ref);
+                final Location location = GPSInfo.getmInstance(context).getGPSLocation();
+                geoFire.getLocation(targetUuid, new LocationCallback() {
+                    @SuppressLint("DefaultLocale")
+                    @Override
+                    public void onLocationResult(String s, GeoLocation geoLocation) {
+                        Location targetLocation = new Location("");//provider name is unnecessary
+                        targetLocation.setLatitude(geoLocation.latitude);//your coords of course
+                        targetLocation.setLongitude(geoLocation.longitude);
+                        final float distance = location.distanceTo(targetLocation);
+                        item.setDistance(distance);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+
+                FirebaseDatabase.getInstance().getReference().child(chat).child(roomName)
+                        .addChildEventListener(new ChildEventListener() {
+                            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                                Log.d("fireabaseUpdate", "onChildAdded:" + dataSnapshot.getKey());
+
+                                // A new comment has been added, add it to the displayed list
+                                MessageVO message = dataSnapshot.getValue(MessageVO.class);
+                                ChatMessage chatMessage;
+                                int check = message.getCheck();
+
+                                if(check!=0 && !message.getWriter().equals(userUuid)){
+                                    message.setCheck(0);
+                                    databaseRef.child(chat).child(roomName).child(dataSnapshot.getKey()).child("check").setValue(check-1);
+                                }
+
+                                if(message.getWriter().equals(userUuid) && message.getImage() == null) {
+                                    chatMessage = new ChatMessage(message, true, false);
+                                }
+                                else if(!message.getWriter().equals(userUuid) && message.getImage() == null){
+                                    chatMessage = new ChatMessage(message, false, false, item);
+                                }
+                                else if(message.getWriter().equals(userUuid) && message.getContent() == null){
+                                    chatMessage = new ChatMessage(message, true, true);
+                                }
+                                else{
+                                    chatMessage = new ChatMessage(message, false, true,item);
+                                }
+                                mAdapter.add(chatMessage);
+                                mAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                                Log.d("fireabaseUpdate", "onChildChanged:" + dataSnapshot.getKey());
+
+                                // A comment has changed, use the key to determine if we are displaying this
+                                // comment and if so displayed the changed comment.
+                                MessageVO user = dataSnapshot.getValue(MessageVO.class);
+                                String commentKey = dataSnapshot.getKey();
+                                mAdapter.notifyDataSetChanged();
+                            }
+                            @Override
+                            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                            }
+
+                            @Override
+                            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public Long getTime() {
+        return System.currentTimeMillis();
+    }
+
+    public String getRoomName(){
+        return roomName;
+    }
+}
+
