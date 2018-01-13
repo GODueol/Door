1624b8d99ba213c679e82455fc7084b50af8eb97
ByoungJin <kbj2043@gmail.com>
2018-01-14 02:06:46 +0900

    코어 글쓰기 액티비티 fab 버튼의 [+] & [x] 버튼 연속 클릭할 시 회전 각도 틀어짐

--- app/src/main/java/com/example/kwoncheolhyeok/core/CorePage/CoreWriteActivity.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/CorePage/CoreWriteActivity.java
@@ -193,7 +193,6 @@ public class CoreWriteActivity extends AppCompatActivity {
                 );
                 recordSelectDialog.show();
 
-                closeFABMenu();
             }
         });
 
