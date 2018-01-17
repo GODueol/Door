63ab8568930a9a376d230945940ec243411a6c21
ByoungJin <kbj2043@gmail.com>
2018-01-18 01:40:55 +0900

    코어 글 쓸때 갤러리 가져오는 방식 바꿈 : 구글드라이브나 SD카드의 이미지도 가져오도록

--- app/src/main/java/com/example/kwoncheolhyeok/core/CorePage/CoreWriteActivity.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/CorePage/CoreWriteActivity.java
@@ -2,19 +2,26 @@ package com.example.kwoncheolhyeok.core.CorePage;
 
 import android.animation.Animator;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.CompoundButton;
@@ -45,15 +52,15 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.yanzhenjie.album.Action;
-import com.yanzhenjie.album.Album;
-import com.yanzhenjie.album.AlbumFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 import cafe.adriel.androidaudiorecorder.AndroidAudioRecorder;
 import cafe.adriel.androidaudiorecorder.model.AudioChannel;
@@ -181,18 +188,26 @@ public class CoreWriteActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                // ===========test =====
+
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("image/*");
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+                } else {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+                }
+                /*
+
                 // 사진파일 가져오기 라이브러리 적용
                 Album.image(CoreWriteActivity.this) // Image selection.
                         .singleChoice()
-//                        .multipleChoice()
                         .requestCode(2)
                         .camera(false)
-//                        .columnCount()
-//                        .selectCount()
-//                        .checkedList(mAlbumFiles)
-//                        .filterSize(30) // Filter the file size.
-//                        .filterMimeType() // Filter file format.
-//                        .afterFilterVisibility() // Show the filtered files, but they are not available.
                         .onResult(new Action<ArrayList<AlbumFile>>() {
                             @Override
                             public void onAction(int requestCode, @NonNull ArrayList<AlbumFile> result) {
@@ -217,6 +232,8 @@ public class CoreWriteActivity extends AppCompatActivity {
                         .start();
 
 //                loadPicture.onGallery();
+                */
+
                 pictureFab_layout.setClickable(false);
 
             }
@@ -454,6 +471,7 @@ public class CoreWriteActivity extends AppCompatActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -463,6 +481,7 @@ public class CoreWriteActivity extends AppCompatActivity {
                 editImageUri = data.getData();
                 editImage.setImageURI(editImageUri);   // Local Set
                 closeFABMenu();
+                return;
             }
             if(requestCode == REQUEST_RECORD){
                 // Great! User has recorded and saved the audio file
@@ -481,8 +500,187 @@ public class CoreWriteActivity extends AppCompatActivity {
                 edit_audio_layout.setVisibility(View.VISIBLE);
 
                 closeFABMenu();
+                return;
+            }
+            if (data != null && data.getData() != null) {
+                boolean isImageFromGoogleDrive = false;
+                Uri uri = data.getData();
+
+                String imgPath = null;
+                ImageView img = null;
+
+                if (DocumentsContract.isDocumentUri(CoreWriteActivity.this, uri)) {
+                    if ("com.android.externalstorage.documents".equals(uri.getAuthority())) {
+                        String docId = DocumentsContract.getDocumentId(uri);
+                        String[] split = docId.split(":");
+                        String type = split[0];
+
+                        if ("primary".equalsIgnoreCase(type)) {
+                            imgPath = Environment.getExternalStorageDirectory() + "/" + split[1];
+                        }
+                        else {
+                            Pattern DIR_SEPORATOR = Pattern.compile("/");
+                            Set<String> rv = new HashSet<>();
+                            String rawExternalStorage = System.getenv("EXTERNAL_STORAGE");
+                            String rawSecondaryStoragesStr = System.getenv("SECONDARY_STORAGE");
+                            String rawEmulatedStorageTarget = System.getenv("EMULATED_STORAGE_TARGET");
+                            if(TextUtils.isEmpty(rawEmulatedStorageTarget))
+                            {
+                                if(TextUtils.isEmpty(rawExternalStorage))
+                                {
+                                    rv.add("/storage/sdcard0");
+                                }
+                                else
+                                {
+                                    rv.add(rawExternalStorage);
+                                }
+                            }
+                            else
+                            {
+                                String rawUserId;
+                                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1)
+                                {
+                                    rawUserId = "";
+                                }
+                                else
+                                {
+                                    String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                                    String[] folders = DIR_SEPORATOR.split(path);
+                                    String lastFolder = folders[folders.length - 1];
+                                    boolean isDigit = false;
+                                    try
+                                    {
+                                        Integer.valueOf(lastFolder);
+                                        isDigit = true;
+                                    }
+                                    catch(NumberFormatException ignored)
+                                    {
+                                    }
+                                    rawUserId = isDigit ? lastFolder : "";
+                                }
+                                if(TextUtils.isEmpty(rawUserId))
+                                {
+                                    rv.add(rawEmulatedStorageTarget);
+                                }
+                                else
+                                {
+                                    rv.add(rawEmulatedStorageTarget + File.separator + rawUserId);
+                                }
+                            }
+                            if(!TextUtils.isEmpty(rawSecondaryStoragesStr))
+                            {
+                                String[] rawSecondaryStorages = rawSecondaryStoragesStr.split(File.pathSeparator);
+                                Collections.addAll(rv, rawSecondaryStorages);
+                            }
+                            String[] temp = rv.toArray(new String[rv.size()]);
+                            for (String aTemp : temp) {
+                                File tempf = new File(aTemp + "/" + split[1]);
+                                if (tempf.exists()) {
+                                    imgPath = aTemp + "/" + split[1];
+                                }
+                            }
+                        }
+                    }
+                    else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                        String id = DocumentsContract.getDocumentId(uri);
+                        Uri contentUri = ContentUris.withAppendedId( Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                        Cursor cursor = null;
+                        String column = "_data";
+                        String[] projection = { column };
+                        try {
+                            cursor = CoreWriteActivity.this.getContentResolver().query(contentUri, projection, null, null,
+                                    null);
+                            if (cursor != null && cursor.moveToFirst()) {
+                                int column_index = cursor.getColumnIndexOrThrow(column);
+                                imgPath = cursor.getString(column_index);
+                            }
+                        } finally {
+                            if (cursor != null)
+                                cursor.close();
+                        }
+                    }
+                    else if("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                        String docId = DocumentsContract.getDocumentId(uri);
+                        String[] split = docId.split(":");
+                        String type = split[0];
+
+                        Uri contentUri = null;
+                        if ("image".equals(type)) {
+                            contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                        } else if ("video".equals(type)) {
+                            contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                        } else if ("audio".equals(type)) {
+                            contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                        }
+
+                        String selection = "_id=?";
+                        String[] selectionArgs = new String[]{ split[1] };
+
+                        Cursor cursor = null;
+                        String column = "_data";
+                        String[] projection = { column };
+
+                        try {
+                            cursor = CoreWriteActivity.this.getContentResolver().query(contentUri, projection, selection, selectionArgs, null);
+                            if (cursor != null && cursor.moveToFirst()) {
+                                int column_index = cursor.getColumnIndexOrThrow(column);
+                                imgPath = cursor.getString(column_index);
+                            }
+                        } finally {
+                            if (cursor != null)
+                                cursor.close();
+                        }
+                    }
+                    else if("com.google.android.apps.docs.storage".equals(uri.getAuthority()))   {
+                        isImageFromGoogleDrive = true;
+                    }
+                }
+                else if ("content".equalsIgnoreCase(uri.getScheme())) {
+                    Cursor cursor = null;
+                    String column = "_data";
+                    String[] projection = { column };
+
+                    try {
+                        cursor = CoreWriteActivity.this.getContentResolver().query(uri, projection, null, null, null);
+                        if (cursor != null && cursor.moveToFirst()) {
+                            int column_index = cursor.getColumnIndexOrThrow(column);
+                            imgPath = cursor.getString(column_index);
+                        }
+                    }
+                    finally {
+                        if (cursor != null)
+                            cursor.close();
+                    }
+                }
+                else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                    imgPath = uri.getPath();
+                }
+
+                if(isImageFromGoogleDrive)  {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                        img.setImageBitmap(bitmap);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                else    {
+
+                    File f = new File(imgPath);
+                    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                    Bitmap originalBitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),bmOptions);
+                    editImageUri = uri;
+                    Bitmap orientedBitmap = BitmapUtil.rotateBitmap(imgPath, originalBitmap);
+                    editImage.setImageBitmap(orientedBitmap);
+                    image_edit_layout.setVisibility(View.VISIBLE);
+                    closeFABMenu();
+
+                }
             }
         }
+
     }
 
     private void uploadPicture() {
