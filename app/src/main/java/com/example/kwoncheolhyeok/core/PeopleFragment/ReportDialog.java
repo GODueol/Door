f6433d331af7397e52f0a21e4896e5178995c50d
ByoungJin <kbj2043@gmail.com>
2018-02-08 20:43:17 +0900

    신고 다이얼로그 넘버피커 텍스트로 변경

--- app/src/main/java/com/example/kwoncheolhyeok/core/PeopleFragment/ReportDialog.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/PeopleFragment/ReportDialog.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.example.kwoncheolhyeok.core.CorePage.CustomDialog;
@@ -27,8 +28,12 @@ public class ReportDialog extends CustomDialog {
 
         TextView cancel = findViewById(R.id.cancel);
         TextView report = findViewById(R.id.report);
+        NumberPicker picker = findViewById(R.id.typePicker);
+        picker.setMinValue(0);
+        picker.setMaxValue(2);
+        picker.setDisplayedValues( new String[] { "Belgium", "France", "United Kingdom" } );
 
-        cancel.setOnClickListener(new View.OnClickListener() {
+                cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dismiss();
@@ -39,6 +44,8 @@ public class ReportDialog extends CustomDialog {
             @Override
             public void onClick(View view) {
                 // 신고
+
+
             }
         });
 
