e7592a551d0f1caf9d09324caf40d978cbdf0428
GODueol <weogjw01@naver.com>
2018-03-19 20:37:59 +0900

    add] 비밀번호 변경 추가

--- app/src/main/java/com/example/kwoncheolhyeok/core/SettingActivity/AccountActivity.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/SettingActivity/AccountActivity.java
@@ -1,21 +1,18 @@
 package com.example.kwoncheolhyeok.core.SettingActivity;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.kwoncheolhyeok.core.LoginActivity.LoginActivity;
 import com.example.kwoncheolhyeok.core.R;
+import com.example.kwoncheolhyeok.core.Util.DataContainer;
+import com.example.kwoncheolhyeok.core.Util.UiUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -23,8 +20,6 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import butterknife.Bind;
 
 /**
  * Created by Kwon on 2018-01-04.
@@ -98,39 +93,50 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
         switch (view.getId()) {
             case R.id.change_pw:
                 final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String email = DataContainer.getInstance().getUser().getEmail();
+                UiUtil.getInstance().startProgressDialog(this);
+
+                // 비밀번호 변경 검증
+                try {
+                    validate();
+                } catch (Exception e) {
+                    UiUtil.getInstance().stopProgressDialog();
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 AuthCredential credential = EmailAuthProvider
-                        .getCredential(user.getEmail(), String.valueOf(currentPw.getText()));
+                        .getCredential(email, String.valueOf(currentPw.getText()));
 
                 user.reauthenticate(credential)
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 //패스워드 인증 성공
-                                if(pw.getText().equals(pwConfirm.getText())){
-                                    String newPassword = String.valueOf(pw.getText());
+                                String newPassword = String.valueOf(pw.getText());
 
-                                    user.updatePassword(newPassword)
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    if (task.isSuccessful()) {
-                                                        Toast.makeText(getApplicationContext(), "패스워드가 변경되었습니다..", Toast.LENGTH_SHORT).show();
-                                                    }
+                                user.updatePassword(newPassword)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(getApplicationContext(), "패스워드가 변경되었습니다.", Toast.LENGTH_SHORT).show();
                                                 }
-                                            }).addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(getApplicationContext(), "새비밀번호가 일치하지 않습니다. ", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                                }
-
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getApplicationContext(), "패스워드 변경에 실패하였습니다. ", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                UiUtil.getInstance().stopProgressDialog();
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        focusEditText(currentPw);
                         Toast.makeText(getApplicationContext(), "현재 비밀번호를 다시 확인해주세요. ", Toast.LENGTH_SHORT).show();
+                        UiUtil.getInstance().stopProgressDialog();
                     }
                 });
                 break;
@@ -167,7 +173,30 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
                         }).show();*/
                 break;
         }
+    }
 
+    public void validate() throws Exception {
+        String currentPassword = currentPw.getText().toString();
+        String password = pw.getText().toString();
+        String reEnterPassword = pwConfirm.getText().toString();
 
+        if (currentPassword.length() < 6 || currentPassword.isEmpty()) {
+            focusEditText(currentPw);
+            throw new Exception("비밀번호를 올바르게 입력해 주세요.");
+        } else if (password.isEmpty() || password.length() < 6 || password.length() > 12) {
+            focusEditText(pwConfirm);
+            focusEditText(pw);
+            throw new Exception("비밀번호는 6자리 이상 12자리 이하로 설정해주세요.");
+        } else if (reEnterPassword.isEmpty() || reEnterPassword.length() < 6 || reEnterPassword.length() > 12 || !(reEnterPassword.equals(password))) {
+            focusEditText(pwConfirm);
+            focusEditText(pw);
+
+            throw new Exception("패스워드가 일치하지 않습니다.");
+        }
+    }
+
+    public void focusEditText(EditText editText) {
+        editText.setText("");
+        editText.requestFocus();
     }
 }
