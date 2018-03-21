087a4b76c03a30c3fd51fc71ad7983d3eb0f13a5
Kwon <rnjscjfgur@naver.com>
2018-03-21 23:43:41 +0900

    세팅 - 비밀번호 변경 수정

--- app/src/main/java/com/example/kwoncheolhyeok/core/SettingActivity/AccountActivity.java
+++ app/src/main/java/com/example/kwoncheolhyeok/core/SettingActivity/AccountActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.kwoncheolhyeok.core.LoginActivity.LoginActivity;
 import com.example.kwoncheolhyeok.core.R;
 import com.example.kwoncheolhyeok.core.Util.DataContainer;
 import com.example.kwoncheolhyeok.core.Util.UiUtil;
@@ -62,6 +64,7 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
         getSupportActionBar().setDisplayShowHomeEnabled(true); //홈 아이콘을 숨김처리합니다.
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_36dp);
+
         forgotPw.setOnClickListener(this);
         changePw.setOnClickListener(this);
         new_password_layout.setVisibility(View.GONE);
@@ -124,13 +127,17 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
                                             @Override
                                             public void onComplete(@NonNull Task<Void> task) {
                                                 if (task.isSuccessful()) {
-                                                    Toast.makeText(getApplicationContext(), "패스워드가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+                                                    currentPw.setText(null);
+                                                    pw.setText(null);
+                                                    pwConfirm.setText(null);
+                                                    new_password_layout.setVisibility(View.GONE);
+                                                    Toast.makeText(getApplicationContext(), "비밀번호가 변경되었습니다.", Toast.LENGTH_LONG).show();
                                                 }
                                             }
                                         }).addOnFailureListener(new OnFailureListener() {
                                     @Override
                                     public void onFailure(@NonNull Exception e) {
-                                        Toast.makeText(getApplicationContext(), "패스워드 변경에 실패하였습니다. ", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getApplicationContext(), "비밀번호 변경에 실패하였습니다. ", Toast.LENGTH_SHORT).show();
                                     }
                                 });
                                 UiUtil.getInstance().stopProgressDialog();
@@ -144,12 +151,20 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
                     }
                 });
                 break;
+
             case R.id.forgot_pw:
                 final AutoCompleteTextView email = new AutoCompleteTextView(this);
                 email.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                TextView title = new TextView(this);
+                title.setText("[비밀번호 찾기]\n\n가입하신 메일을 알려주세요.\n비밀번호 변경 페이지를 메일로 보내드립니다.");
+                title.setGravity(Gravity.CENTER);
+                title.setPadding(0,90,0,40);
+                title.setTextSize(15);
+
                 builder.setView(email)
-                        .setTitle("이메일을 쓰세요")
+                        .setCustomTitle(title)
                         .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
@@ -162,15 +177,15 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
                                                 @Override
                                                 public void onComplete(@NonNull Task<Void> task) {
                                                     if (task.isSuccessful()) {
-                                                        Toast.makeText(getApplicationContext(), "이메일 전송 완료", Toast.LENGTH_SHORT).show();
+                                                        Toast.makeText(getApplicationContext(), "메일 전송 완료", Toast.LENGTH_SHORT).show();
                                                     } else {
-                                                        Toast.makeText(getApplicationContext(), "이메일 주소가 올바르지 않습니다", Toast.LENGTH_SHORT).show();
+                                                        Toast.makeText(getApplicationContext(), "메일 주소가 올바르지 않습니다", Toast.LENGTH_SHORT).show();
                                                     }
 
                                                 }
                                             });
                                 } else {
-                                    Toast.makeText(getApplicationContext(), "이메일을 쓰세요", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "다시 시도해주세요", Toast.LENGTH_SHORT).show();
                                 }
 
                             }
@@ -195,7 +210,7 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
             focusEditText(pwConfirm);
             focusEditText(pw);
 
-            throw new Exception("패스워드가 일치하지 않습니다.");
+            throw new Exception("비밀번호가 일치하지 않습니다.");
         }
     }
 
