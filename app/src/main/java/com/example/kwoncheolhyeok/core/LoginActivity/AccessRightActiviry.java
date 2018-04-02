b33c60e6dab75d85e289cd0ff6b792aca2b8e0c8
Kwon <rnjscjfgur@naver.com>
2018-04-02 22:20:55 +0900

    권한 접근 안내 페이지 생성

---
+++ app/src/main/java/com/example/kwoncheolhyeok/core/LoginActivity/AccessRightActiviry.java
@@ -0,0 +1,26 @@
+package com.example.kwoncheolhyeok.core.LoginActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.example.kwoncheolhyeok.core.R;
+
+/**
+ * Created by Kwon on 2018-04-02.
+ */
+
+public class AccessRightActiviry extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.access_right_main);
+
+
+    }
+
+
+}
