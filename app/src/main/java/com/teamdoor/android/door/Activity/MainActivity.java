95fd1a73465ae430d2a894e49f4591771766007c
GODueol <weogjw01@naver.com>
2018-07-05 13:37:13 +0900

    test] Update

--- app/src/main/java/com/teamdoor/android/door/Activity/MainActivity.java
+++ app/src/main/java/com/teamdoor/android/door/Activity/MainActivity.java
@@ -297,7 +297,6 @@ public class MainActivity extends BaseActivity
                 e.printStackTrace();
             }
         });
-        Toast.makeText(this,"test3",Toast.LENGTH_SHORT).show();
         navAlarmDialog = new NavAlarmDialog(MainActivity.this);
         showWeeklyTopicDialog();
     }
